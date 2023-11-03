ea121a4ade91366446fee4ea58f2f790bd014fe7
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-03 03:20:38 +0300

    add brain-calc.js and index.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import runGame from '../src/index.js';
+
+const description = 'What is the result of the expression?';
+const questionsAndAnswers = [];
+
+for (let i = 0; i < 3; i += 1) {
+    const firstNum = Math.floor(Math.random() * 100) + 1;
+    const secondNum = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    let question, answer;
+
+    switch (randomNumber) {
+        case 1:
+            question = `${firstNum} + ${secondNum}`;
+            answer = String(firstNum + secondNum);
+            break;
+        case 2:
+            question = `${firstNum} - ${secondNum}`;
+            answer = String(firstNum - secondNum);
+            break;
+        case 3:
+            question = `${firstNum} * ${secondNum}`;
+            answer = String(firstNum * secondNum);
+            break;
+    }
+
+    questionsAndAnswers.push([question, answer]);
+}
+
+runGame(description, questionsAndAnswers);
+
+
+
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,28 +1,15 @@
 #!/usr/bin/env node
 
-import readlineSync from 'readline-sync';
+import runGame from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-let success = true;
-const min = 1;
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const questionsAndAnswers = [];
 const max = 100;
+const min = 1;
 for (let i = 0; i < 3; i += 1) {
-    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
     const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === isEven) {
-        console.log('Correct!');
-    } else {
-        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'`);
-        console.log(`Let's try again, ${name}!`);
-        success = false;
-        break; 
-    }
+    questionsAndAnswers.push([String(randomNumber), isEven]);
 }
-if (success) {
-    console.log(`Congratulations, ${name}!`);
-}
+
+runGame(description, questionsAndAnswers);
