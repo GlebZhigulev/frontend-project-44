46aeb0d93868ba9b787b5e58b9821b7008719ad6
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-13 23:44:03 +0300

    change brain-prime game

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js';
+import runGame from "../src/index.js";
 
-const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const description =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
 const questionsAndAnswers = [];
 
 const isPrime = (number) => {
-    if (number <= 1) {
-        return false; 
+  if (number <= 1) {
+    return false;
+  }
+  for (var i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
-    for (var i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false; 
-        }
-    }
-    return true; 
-}
+  }
+  return true;
+};
 
 for (let i = 0; i < 3; i += 1) {
-
-    const number = Math.floor(Math.random() * 100) + 1;
-    const question = String(number);
-    const answer = isPrime(number) ? 'yes' : 'no';
-    questionsAndAnswers.push([question, answer]);
+  const number = Math.floor(Math.random() * 100) + 1;
+  const question = String(number);
+  const answer = isPrime(number) ? "yes" : "no";
+  questionsAndAnswers.push([question, answer]);
 }
 runGame(description, questionsAndAnswers);
