ebbe245c8cfd94e429eb408ee8287ba531654ebe
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-13 23:45:10 +0300

    change brain-gcd game

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js';
+import runGame from "../src/index.js";
 
-const description = 'Find the greatest common divisor of given numbers.';
+const description = "Find the greatest common divisor of given numbers.";
 const questionsAndAnswers = [];
 
 const findCdg = (num1, num2) => {
-    while (num2) {
-        let temp = num2;
-        num2 = num1 % num2;
-        num1 = temp;
-    }
-    return num1;
+  while (num2) {
+    let temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+  return num1;
 };
 
 for (let i = 0; i < 3; i += 1) {
-    const firstNum = Math.floor(Math.random() * 100) + 1;
-    const secondNum = Math.floor(Math.random() * 100) + 1;
-    
-    const question = `${firstNum} ${secondNum}`;
-    const answer = String(findCdg(firstNum, secondNum));
+  const firstNum = Math.floor(Math.random() * 100) + 1;
+  const secondNum = Math.floor(Math.random() * 100) + 1;
 
-    questionsAndAnswers.push([question, answer]);
-};
+  const question = `${firstNum} ${secondNum}`;
+  const answer = String(findCdg(firstNum, secondNum));
+
+  questionsAndAnswers.push([question, answer]);
+}
 
 runGame(description, questionsAndAnswers);
