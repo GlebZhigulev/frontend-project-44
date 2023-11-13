42ab023218f1ff818d1f0cbea22ba870a0b4a1ea
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-13 23:45:33 +0300

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,33 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const game = (description, questionsAndAnswers) => {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-    let success = true;
+  console.log("Welcome to the Brain Games!");
+  const name = readlineSync.question("May I have your name? ");
+  console.log(`Hello, ${name}!`);
+  let success = true;
 
-    console.log(description);
+  console.log(description);
 
-    for (let i = 0; i < questionsAndAnswers.length; i += 1) {
-        const [question, answer] = questionsAndAnswers[i];
-        console.log(`Question: ${question}`);
-        const yourAnswer = readlineSync.question('Your answer: ');
+  for (let i = 0; i < questionsAndAnswers.length; i += 1) {
+    const [question, answer] = questionsAndAnswers[i];
+    console.log(`Question: ${question}`);
+    const yourAnswer = readlineSync.question("Your answer: ");
 
-        if (answer === yourAnswer) {
-            console.log('Correct!');
-        } else {
-            console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${answer}'`);
-            console.log(`Let's try again, ${name}!`);
-            success = false;
-            break;
-        }
+    if (answer === yourAnswer) {
+      console.log("Correct!");
+    } else {
+      console.log(
+        `'${yourAnswer}' is wrong answer ;(. Correct answer was '${answer}'`,
+      );
+      console.log(`Let's try again, ${name}!`);
+      success = false;
+      break;
     }
+  }
 
-    if (success) {
-        console.log(`Congratulations, ${name}!`);
-    }
-}
+  if (success) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
 
 export default game;
