1f415c0da0903c32bfd9ad9555f43df48eff8120
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-13 21:03:34 +0300

    change brain-prime game

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -9,23 +9,19 @@ const isPrime = (number) => {
     if (number <= 1) {
         return false; 
     }
-
     for (var i = 2; i <= Math.sqrt(number); i++) {
         if (number % i === 0) {
             return false; 
         }
     }
-
     return true; 
 }
 
 for (let i = 0; i < 3; i += 1) {
-    
+
     const number = Math.floor(Math.random() * 100) + 1;
     const question = String(number);
     const answer = isPrime(number) ? 'yes' : 'no';
-
     questionsAndAnswers.push([question, answer]);
 }
-
 runGame(description, questionsAndAnswers);
