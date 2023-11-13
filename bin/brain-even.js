b4ddd94870c0bf325302ac32cd7f6fb320f64fdf
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-11-13 23:44:55 +0300

    change brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
-import runGame from '../src/index.js';
+import runGame from "../src/index.js";
 
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const description =
+  'Answer "yes" if the number is even, otherwise answer "no".';
 const questionsAndAnswers = [];
 const max = 100;
 const min = 1;
 for (let i = 0; i < 3; i += 1) {
-    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    const isEven = randomNumber % 2 === 0 ? 'yes' : 'no';
-    questionsAndAnswers.push([String(randomNumber), isEven]);
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  const isEven = randomNumber % 2 === 0 ? "yes" : "no";
+  questionsAndAnswers.push([String(randomNumber), isEven]);
 }
 
 runGame(description, questionsAndAnswers);
