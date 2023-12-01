fb54e94196dfff82e13b5815d1827c23e9bce876
Gleb Zhigulev <glebzhigulevv@gmail.com>
2023-12-01 04:35:39 +0300

    modifing project

--- src/games/randomizer.js
+++ src/games/randomizer.js
@@ -1,6 +1,10 @@
 const getRandomNumberUpToMax = (toNum) => Math.floor(Math.random() * toNum) + 1;
 
-const getRandomNumberBetweenMinAndMax  = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumberBetweenMinAndMax = (min, max) => {
+  const range = max - min + 1;
+  const random = Math.floor(Math.random() * range);
+  return random + min;
+};
 
 export { getRandomNumberUpToMax };
 export { getRandomNumberBetweenMinAndMax };
